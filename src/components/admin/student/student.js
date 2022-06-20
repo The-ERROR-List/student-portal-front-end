dfcc0a12a71b339b09a11466908d0cf2db94aca0
WalidAlrefai <w.alrefai.3@gmail.com>
2022-06-20 14:04:52 +0300

    add form

---
+++ src/components/admin/student/student.js
@@ -0,0 +1,242 @@
+import { Table, Form, Row, Col, FormGroup, Label, Input } from 'reactstrap'
+import { Button, Modal } from 'react-bootstrap'
+import { useState } from 'react'
+import './student.scss'
+function Submit() {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    return (
+        <>
+            <Button variant="primary" onClick={handleShow}>
+                Add Student
+            </Button>
+
+            <Modal show={show} onHide={handleClose} class="modal-dialog modal-lg">
+                <Modal.Header closeButton>
+                    <Modal.Title>Student form</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form style={{ width: "70%", margin: "auto" }} >
+                        <Row>
+                            <Col md={6}>
+                                <FormGroup>
+                                    <Label for="userName">userName</Label>
+                                    <Input
+                                        id="userName"
+                                        name="userName"
+                                        placeholder="userName..."
+                                        type="userName"
+                                    />
+                                </FormGroup>
+                                <FormGroup>
+                                    <Label for="exampleEmail">Email</Label>
+                                    <Input
+                                        id="email"
+                                        name="email"
+                                        // value="email"
+                                        placeholder="Email..."
+                                        type="email"
+                                    />
+                                </FormGroup>
+                            </Col>
+                            <Col md={6}>
+                                <FormGroup>
+                                    <Label for="examplePassword">Password</Label>
+                                    <Input
+                                        id="examplePassword"
+                                        name="password"
+                                        placeholder="password placeholder"
+                                        type="password"
+                                    />
+                                </FormGroup>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col md={6}>
+                                <FormGroup>
+                                    <Label for="firstName">First Name</Label>
+                                    <Input
+                                        id="firstName"
+                                        name="firstName"
+                                        placeholder="First Name..."
+                                    />
+                                </FormGroup>
+                                <FormGroup>
+                                    <Label for="lastName">Last Name </Label>
+                                    <Input
+                                        id="lastName"
+                                        name="lastName"
+                                        placeholder="Last Name...."
+                                    />
+                                </FormGroup>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col md={3}>
+                                <FormGroup>
+                                    <Label for="role">role</Label>
+                                    {/* <div class="dropdown">
+                                        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                                            role
+                                        </button>
+                                        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                                            <li>Teacher</li>
+                                            
+                                        </ul>
+                                        </div> */}
+                                    <Input
+                                        id="role"
+                                        name="role"
+                                        placeholder="role"
+
+                                    />
+                                </FormGroup>
+                            </Col>
+                            <Col md={3}>
+                                <FormGroup>
+                                    <Label for="gender">gender</Label>
+                                    <Input
+                                        id="gender"
+                                        name="gender"
+                                        placeholder="gender"
+
+                                    />
+                                </FormGroup>
+                            </Col>
+                            <Col md={6}>
+                                <FormGroup>
+                                    <Label for="nationality">nationality</Label>
+                                    <Input
+                                        id="nationality"
+                                        name="nationality"
+                                        placeholder="nationality"
+
+                                    />
+                                </FormGroup>
+                            </Col>
+                        </Row>
+
+                        <FormGroup>
+                            <Label for="major">major</Label>
+                            <Input
+                                id="major"
+                                name="major"
+                                placeholder="major"
+
+                            />
+                        </FormGroup>
+
+                        {/* <Button>Add</Button> */}
+                    </Form>
+
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={handleClose}>
+                        Add Student
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    );
+}
+const Teacher = () => {
+    return (
+        <div className="teacher">
+            <h1>Student</h1>
+            <Submit />
+            <Table className="teacher-table">
+                <thead>
+                    <tr>
+                        <th>
+                            #
+                        </th>
+                        <th>
+                            First Name
+                        </th>
+                        <th>
+                            Last Name
+                        </th>
+                        <th>
+                            Username
+                        </th>
+                        <th>
+                            Nationality
+                        </th>
+                        <th>
+                            Major
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">
+                            1
+                        </th>
+                        <td>
+                            Mark
+                        </td>
+                        <td>
+                            Otto
+                        </td>
+                        <td>
+                            @mdo
+                        </td>
+                        <td>
+                            Otto
+                        </td>
+                        <td>
+                            @mdo
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">
+                            2
+                        </th>
+                        <td>
+                            Jacob
+                        </td>
+                        <td>
+                            Thornton
+                        </td>
+                        <td>
+                            @fat
+                        </td>
+                        <td>
+                            Otto
+                        </td>
+                        <td>
+                            @mdo
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">
+                            3
+                        </th>
+                        <td>
+                            Larry
+                        </td>
+                        <td>
+                            the Bird
+                        </td>
+                        <td>
+                            @twitter
+                        </td>
+                        <td>
+                            Otto
+                        </td>
+                        <td>
+                            @mdo
+                        </td>
+                    </tr>
+                </tbody>
+            </Table>
+        </div>
+    )
+}
+export default Teacher;
