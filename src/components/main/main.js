b956cce010d19419f583a026a69abd53da6cdcb7
WalidAlrefai <w.alrefai.3@gmail.com>
2022-06-20 22:48:09 +0300

    render main component

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -1,8 +1,11 @@
 import Header from '../header/header';
-import {AuthContext} from '../../context/Auth';
-import  {When}  from 'react-if';
-import {useContext} from 'react';
+import { AuthContext } from '../../context/Auth';
+import { When } from 'react-if';
+import { useContext } from 'react';
 import Admin from '../admin/Admin'
+import Student from '../student/Student';
+import Teacher from '../teacher/Teacher';
+import './main.scss';
 function Main() {
     const auth = useContext(AuthContext);
 
@@ -10,9 +13,19 @@ function Main() {
         <div className="main">
             <When condition={auth.isLoggedIn}>
                 <Header />
-                <Admin/>
+                <div className="main-content">
+                <When condition={auth.user.role === 'admin'}>
+                    <Admin />
+                </When>
+                <When condition={auth.user.role === 'student'}>
+                    <Student />
+                </When>
+                <When condition={auth.user.role === 'teacher'}>
+                    <Teacher />
+                </When>
+                </div>
             </When>
-            
+
         </div>
     );
 }
