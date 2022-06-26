374d45014feb3939e3fef8873e927f0c0b93e452
ibrahimalaqoul <alaqoulibrahim@gmail.com>
2022-06-27 01:01:14 +0300

    editing update course

--- src/components/admin/course/course-teacher.js
+++ src/components/admin/course/course-teacher.js
@@ -27,20 +27,23 @@ import { deleteCourse } from '../../../redux/type';
 import { StateContext } from "../../../context/State";
 import EditIcon from "@mui/icons-material/Edit";
 import { Modal } from "react-bootstrap";
-  import { getCourseAction } from "../../../redux/course";
-  import { addCourse } from "../../../redux/type";
-  import { api } from '../../../redux/type';
-  
-  import "./course.scss";
-  
+import { api } from '../../../redux/type';
+
+import "./course.scss";
+
 export default function Course_Card(props) {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   console.log(props.course, '++++++++++');
   const selector = useSelector(
     (state) => state.teacherTocourse.teacherIntoCourse
   );
   const [ids, setId] = useState({ id: '' });
   const [infoUpdate, setInfoUpdate] = useState({
-    courseName:"", courseGrade:"", courseDescription: "", courseImg:""
+    courseName: "", courseGrade: "", courseDescription: "", courseImg: ""
   });
   const dispatch = useDispatch();
   const handelChange = (e) => {
@@ -55,17 +58,22 @@ export default function Course_Card(props) {
   }
   function updateUser(e) {
     e.preventDefault();
-    fetch(`${api}/teacher/${ids.id}`, {
+    fetch(`${api}/courses/${ids.id}`, {
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
         "Authorization": `Bearer ${cookie.load("token")}`,
       },
       body: JSON.stringify({
-       courseName:infoUpdate.courseName, courseGrade:infoUpdate.courseGrade, courseDescription: infoUpdate.courseDescription, courseImg:infoUpdate.courseImg,
+        courseName: infoUpdate.courseName,
+        courseGrade: infoUpdate.courseGrade,
+        courseDescription: infoUpdate.courseDescription,
+        courseImg: infoUpdate.courseImg,
       })
     }).then((res) => res.json().then((data) => { console.console(data) }));
-    state.handleClose();
+    console.log('infoooooooooo',infoUpdate)
+
+    handleClose();
   }
   const state = useContext(StateContext);
 
@@ -73,79 +81,84 @@ export default function Course_Card(props) {
     dispatch({ type: deleteCourse, payloadDelete: idToDelete })
   }
 
+ 
   useEffect(() => {
-    dispatch(getAllCourse2Teacher(props.course.id));
+    const interval = setInterval(() => {
+      dispatch(getAllCourse2Teacher(props.course.id));
+    }, 2000);
+    return () => clearInterval(interval);
   }, []);
+
 
   return (
     <div>
       <CardGroup style={{ width: "420px" }}>
         <Card>
-        <Modal
-          show={state.show}
-          onHide={state.handleClose}
-          class="modal-dialog modal-lg"
-        >
-          <Modal.Header closeButton>
-            <Modal.Title>Course form</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <Form style={{ width: "70%", margin: "auto" }}>
-              <Row>
-                <Col md={12}>
-                  <FormGroup>
-                    <Label for="courseName">Course Name</Label>
-                    <Input
-                      id="course"
-                      name="courseName"
-                      placeholder="Course..."
-                      type="text"
-                      onChange={handelChange}
-                    />
-                  </FormGroup>
-                </Col>
-              </Row>
-              <FormGroup>
-                <Label for="GradeCourse">Grade course</Label>
-                <Input
-                  id="Grade"
-                  name="courseGrade"
-                  placeholder="Grade..."
-                  type="number"
-                  onChange={handelChange}
-                />
-              </FormGroup>
-              <FormGroup>
-                <Label for="courseDescription">course Description</Label>
-                <Input
-                  id="courseDescription"
-                  name="courseDescription"
-                  placeholder="course Description..."
-                  type="text"
-                  onChange={handelChange}
-                />
-              </FormGroup>
-              <FormGroup>
-              <Label for="courseImg">Course Img</Label>
-                <Input
-                  id="courseImg"
-                  name="courseImg"
-                  placeholder="Image..."
-                  type="text"
-                  onChange={handelChange}
-                />
-              </FormGroup>
-              <Button color="success" onClick={(e) =>updateUser(e)}>
-                Add Course
+          <Modal
+            show={show}
+            onHide={handleClose}
+            class="modal-dialog modal-lg"
+          >
+            <Modal.Header closeButton>
+              <Modal.Title>Course form</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <Form style={{ width: "70%", margin: "auto" }}>
+                <Row>
+                  <Col md={12}>
+                    <FormGroup>
+                      <Label for="courseName">Course Name</Label>
+                      <Input
+                        id="course"
+                        name="courseName"
+                        placeholder="Course..."
+                        type="text"
+                        onChange={handelChange}
+                      />
+                    </FormGroup>
+                  </Col>
+                </Row>
+                <FormGroup>
+                  <Label for="GradeCourse">Grade course</Label>
+                  <Input
+                    id="Grade"
+                    name="courseGrade"
+                    placeholder="Grade..."
+                    type="number"
+                    onChange={handelChange}
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <Label for="courseDescription">course Description</Label>
+                  <Input
+                    id="courseDescription"
+                    name="courseDescription"
+                    placeholder="course Description..."
+                    type="text"
+                    onChange={handelChange}
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <Label for="courseImg">Course Img</Label>
+                  <Input
+                    id="courseImg"
+                    name="courseImg"
+                    placeholder="Image..."
+                    type="text"
+                    onChange={handelChange}
+                  />
+                </FormGroup>
+                <Button color="success" onClick={updateUser}>
+                  update Course
+                </Button>
+              </Form>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button color="danger" onClick={handleClose}>
+                Close
               </Button>
-            </Form>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button color="danger" onClick={state.handleClose}>
-              Close
-            </Button>
-          </Modal.Footer>
-        </Modal>
+            </Modal.Footer>
+          </Modal>
           <CardImg
             top
             src={props.course.courseImg}
@@ -158,12 +171,12 @@ export default function Course_Card(props) {
             </CardTitle>
             <DeleteIcon sx={{ fontSize: 50 }} onClick={() => deleteFromDB(props.course.id)} />
             <EditIcon
-                        sx={{ fontSize: 67 }}
-                        onClick={() => {
-                          idUser(props.course.id)
-                          state.handleShow()
-                        }}
-                      />
+              sx={{ fontSize: 67 }}
+              onClick={() => {
+                idUser(props.course.id)
+                handleShow()
+              }}
+            />
           </CardBody>
           <Accordion defaultActiveKey="0" style={{ width: "400px" }}>
             <Accordion.Item eventKey="0">
@@ -176,10 +189,7 @@ export default function Course_Card(props) {
                 </ul>
               </Accordion.Body>
             </Accordion.Item>
-            <Accordion.Item eventKey="1">
-              <Accordion.Header>Classes</Accordion.Header>
-              <Accordion.Body></Accordion.Body>
-            </Accordion.Item>
+           
           </Accordion>
         </Card>
       </CardGroup>
