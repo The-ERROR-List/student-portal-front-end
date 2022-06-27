fe869a9348b4f698f67ca628c5bf14020eba3d55
nour-alsatari <nouress98@gmail.com>
2022-06-27 04:38:40 +0300

    socket

--- src/components/chat/chat.js
+++ src/components/chat/chat.js
@@ -1,77 +1,103 @@
+// import './App.css';
+import "./styling.css";
 import { useState, useEffect } from "react";
-import ScrollToBottom from "react-scroll-to-bottom";
 
-
-export default function Chat(props) {
+function Chat(props) {
   const [currentMessage, setCurrentMessage] = useState("");
-  const [messageList, setMessageList] = useState([])
+  const [messageList, setMessageList] = useState([]);
 
   const sendMessage = async () => {
-        if (currentMessage !== "") {
-          const messageData = {
-            room: props.room,
-            author: props.username,
-            message: currentMessage,
-            time:
-              new Date(Date.now()).getHours() +
-              ":" +
-              new Date(Date.now()).getMinutes(),
-          };
-          await props.socket.emit("send_message", messageData)
-          setMessageList((list) => [...list, messageData]);// save the message im sending
-          setCurrentMessage("") //reset the message
-        }
+    if (currentMessage !== "") {
+      const messageData = {
+        to: props.socket.id,
+        room: props.room,
+        author: props.username,
+        message: currentMessage,
+        time:
+          new Date(Date.now()).getHours() +
+          ":" +
+          new Date(Date.now()).getMinutes(),
+      };
+      await props.socket.emit("send_message", messageData)
+    //   setMessageList((list) => [...list, messageData]);// save the message im sending
+      setMessageList(messageList.concat(messageData))
 
-
+      setCurrentMessage("") //reset the message
     }
-
+  };
 
   useEffect(() => {
-    props.socket.on('receive_message', (data)=>{
-        console.log(data)
-        setMessageList((list) => [...list, data]); // save the message im recieving
-    })
+    props.socket.on("receive_message", (data) => {
+      console.log(data);
+    //   setMessageList([...messageList, data])
+      setMessageList(messageList.concat(data))
+    });
   }, [props.socket]); // whenever theres a change on the socket server im listening to an event
 
+  useEffect(() => {
+    console.log(messageList);
+  }, [messageList]);
   return (
-    <div className="chat-window">
-      <div className="chat-header">
-        <p> Live Chat</p>{" "}
-      </div>
-
-      <div className="chat-body"></div>
-      <ScrollToBottom className="message-container">
+    <div id="container">
+      <main>
+        <header>
+          <img
+            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/chat_avatar_01.jpg"
+            alt=""
+          />
+          <div>
+            <h2>Chat with Vincent Porter</h2>
+            <h3>already 1902 messages</h3>
+          </div>
+          <img
+            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/ico_star.png"
+            alt=""
+          />
+        </header>
+        <ul id="chat">
           {messageList.map((messageContent) => {
             return (
-              <div
-                className="message"
-                id={props.username === messageContent.author ? "you" : "other"}
+              <li
+              // className={props.username === messageContent.author ? "me" : "you"}
               >
-                <div>
-                  <div className="message-content">
-                    <p>{messageContent.message}</p>
-                  </div>
-                  <div className="message-meta">
-                    <p id="time">{messageContent.time}</p>
-                    <p id="author">{messageContent.author}</p>
-                  </div>
+                <div className="entete">
+                  <span className="status green"></span>
+                  <h2>{messageContent.author}</h2>
+                  <h3>{messageContent.time}</h3>
                 </div>
-              </div>
+                <div className="triangle"></div>
+                <div className="message">{messageContent.message}</div>
+              </li>
             );
           })}
-        </ScrollToBottom>
-      <div className="chat-footer">
-        <input
-          type="text"
-          placeholder="Hello.."
-          value={currentMessage}
-          onChange={(e) => setCurrentMessage(e.target.value)}
-          onKeyPress={(event) => {
-            event.key === "Enter" && sendMessage();
-          }}
-        />
-        <button onClick={sendMessage}> send</button>
-      </div>
+        </ul>
+
+        <footer>
+          <button onClick={sendMessage} style={{ color: "red" }}>
+            {" "}
+            send
+          </button>
+
+          <textarea
+            placeholder="Type your message"
+            value={currentMessage}
+            onChange={(e) => setCurrentMessage(e.target.value)}
+          ></textarea>
+          {/* <img
+            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/ico_picture.png"
+            alt=""
+          />
+          <img
+            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/ico_file.png"
+            alt=""
+          /> */}
+
+          <a href="#">Send</a>
+         
+        </footer>
+      </main>
     </div>
   );
 }
+
+export default Chat;
--- src/components/chat/joinChat.js
+++ src/components/chat/joinChat.js
@@ -1,11 +1,8 @@
-import { useEffect, useState } from "react";
-import Chat from "./chat.js";
-import "./chat.css";
+// import './App.css';
+import { useState, useEffect } from "react";
 import { io } from "socket.io-client";
-import{api} from'../../redux/type'
-import cookie from 'react-cookies'
-import { When } from "react-if";
-const socket = io(`${api}/socketMessages`, {
+import Chat from "./chat";
+const socket = io(`http://localhost:3000/socketMessages`, {
   transports: ["websocket"],
   // reconnectionDelayMax: 10000,
   // auth: {
@@ -15,64 +12,64 @@ const socket = io(`${api}/socketMessages`, {
   //   "my-key": "my-value"
   // }
 });
-
 socket.on("connect", () => {
-  console.log("hello im connected");
-  //   connectMessage("Connected to server  on socket-id :  " + socket.id);
-});
+    console.log("hello im connected");
+    //   connectMessage("Connected to server  on socket-id :  " + socket.id);
+  });
 
-export default function JoinChat() {
-  const [userName, setUsername] = useState("");
-  const [room, setRoom] = useState("");
-  const [showChat, setShowChat] = useState(false);
-
-  function joinRoom() {
-    if(cookie.load('username')!==null){
-      setUsername(cookie.load('username'))
-      setRoom(socket.id)//room id is username
+function Join() {
+    const [userName, setUsername] = useState("");
+    const [room, setRoom] = useState("");
+    const [showChat, setShowChat] = useState(false);
+  
+    function joinRoom() {
+        setUsername('username')
+        setRoom("classId")//room id is classId 
+      
+      if (userName !== "" && room !== "") {
+        socket.emit("join_room", room);
+          setShowChat(true);
+      }
+  
     }
-    if (userName !== "" && room !== "") {
-      socket.emit("join_room", room);
-        setShowChat(true);
-    }
+  
+    useEffect (()=> {
+      console.log('your room is', room);
+    }, [room])
 
-  }
+    return (
 
-  useEffect (()=> {
-    console.log('your room is', room);
-  }, [room])
-
-  return (
-
-    <div className="App">
-    {!showChat ? (
-      <div className="joinChatContainer">
-
-        <When condition={cookie.load('role')==='teacher'}>
-        <h3>chat with your student</h3>
-        </When>
-        <When condition={cookie.load('role')==='student'}>
-        <h3>chat with your Teacher</h3>
-        </When>
-        {/* <input
-          type="text"
-          placeholder="John..."
-          onChange={(event) => {
-            setUsername(event.target.value);
-          }} */}
-        {/* /> */}
-        {/* <input
-          type="text"
-          placeholder="Room ID..."
-          onChange={(event) => {
-            setRoom(event.target.value);
-          }} */}
-        {/* /> */}
-        <button onClick={joinRoom}>Join A Room</button>
+        <div className="App">
+        {!showChat ? (
+          <div className="joinChatContainer">
+    
+            {/* <When condition={cookie.load('role')==='teacher'}> */}
+            <h3>chat with your student</h3>
+            {/* </When> */}
+            {/* <When condition={cookie.load('role')==='student'}> */}
+            <h3>chat with your Teacher</h3>
+            {/* </When> */}
+            {/* <input
+              type="text"
+              placeholder="John..."
+              onChange={(event) => {
+                setUsername(event.target.value);
+              }} */}
+            {/* /> */}
+            {/* <input
+              type="text"
+              placeholder="Room ID..."
+              onChange={(event) => {
+                setRoom(event.target.value);
+              }} */}
+            {/* /> */}
+            <button onClick={joinRoom}>Join A Room</button>
+          </div>
+        ) : (
+          <Chat socket={socket} username={userName} room={room} />
+        )}
       </div>
-    ) : (
-      <Chat socket={socket} username={userName} room={room} />
-    )}
-  </div>
-    );
+        );
 }
+
+export default Join;
