f97e14a5bb2ad0239065ae422e9c74a3b961caa1
WalidAlrefai <w.alrefai.3@gmail.com>
2022-06-27 15:44:29 +0300

    styling

--- src/components/header/course.js
+++ src/components/header/course.js
@@ -1,24 +1,82 @@
 import LibraryBooksOutlinedIcon from '@mui/icons-material/LibraryBooksOutlined';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import './header.scss'
-import { useState } from 'react';
+import { useState, useEffect, useContext } from 'react';
+import { When } from 'react-if';
+import cookie from 'react-cookies';
+import { useSelector, useDispatch } from "react-redux"
+import { getClassesInStudentsAction } from "../../redux/classesInStudents"
+import { StateContext } from "../../context/State";
+
+import { Link } from 'react-router-dom'
+
 
 export default function Course() {
+    const selector = useSelector(state => state.classesInStudents.classesStudent)
     const [show, setShow] = useState(false);
+    const state = useContext(StateContext);
+
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+    const dispatch = useDispatch();
+    useEffect(() => {
 
+        // dispatch(getClassesInStudentsAction())
+        state.getCourses()
+        console.log({state})
+
+    }, [show])
     return (
         <>
             <LibraryBooksOutlinedIcon onClick={handleShow} fontSize="large" sx={{ fontSize: 30 }} color="white" /><p>Course</p>
 
-            <Offcanvas show={show} onHide={handleClose} style={{marginLeft:"140px"}}>
+            <Offcanvas show={show} onHide={handleClose} style={{ marginLeft: "140px" }}>
                 <Offcanvas.Header closeButton>
-                    <Offcanvas.Title>course</Offcanvas.Title>
+                    <When condition={cookie.load('role') === 'teacher'}>
+                        <Offcanvas.Title>Course</Offcanvas.Title>
+                    </When>
+                    <When condition={cookie.load('role') === 'student'}>
+                        <Offcanvas.Title>Classes</Offcanvas.Title>
+                    </When>
+
                 </Offcanvas.Header>
                 <Offcanvas.Body>
+                    <hr />
+                    <br />
+                    {/* <When condition={cookie.load('role') === 'student'}>
+                        {
+                            selector.classes ?
+                                selector.classes.map((classs, i) => {
 
+                                    return (
+                                        <ul>
+                                            <Link to={`/class-student/${classs.classId}`}>
+                                                <li> {classs.className}</li>
+                                            </Link>
+                                        </ul>
+
+                                    )
+                                }) : null
+                        }
+                    </When> */}
+                    
+                    <When condition={cookie.load('role') === 'teacher'}>
+                    <p>hello</p>
+                        {
+                            state.courses.TeacherCourses ?
+                                state.courses.TeacherCourses.map((course, i) => {
+                                    
+                                    return (
+                                        <ul>
+                                            <Link to={`/course/${course['course id']}`} >
+                                                <li onClick={() => window.location.reload()}> {course['course Name']}</li>
+                                            </Link>
+                                        </ul>
+                                    )
+                                }) : null
+                        }
+                    </When>
                 </Offcanvas.Body>
             </Offcanvas>
         </>
--- src/components/teacher/Teacher.js
+++ src/components/teacher/Teacher.js
@@ -1,53 +1,53 @@
 import React, { useEffect } from "react";
 import { useContext } from "react";
-import { Row, Col, Card, CardTitle, CardText, Button, CardBody,CardImg } from "reactstrap";
-// import Card from 'react-bootstrap/Card';
-import {Link} from 'react-router-dom';
+import { Row, Col, Card, CardTitle, CardText, Button, CardBody, CardImg,Alert } from "reactstrap";
+import { Link } from 'react-router-dom';
 import { StateContext } from "../../context/State";
 import { When } from "react-if";
+import './teacher.scss'
 const Teacher = () => {
-  // const [course,setCourse]=useState([]);
   const state = useContext(StateContext);
   //'/get-allCourses-for-teacher/:id '
 
   useEffect(() => {
     state.getCourses()
   }, [])
-  // const teachersCourses = state.getCourses();
   return (
 
-    <div style={{marginRight:"70px"}}>
-      <Card >
-        <CardBody>
-          <CardText>
-            {
-              `welcome ${state.courses.TeacherName}`
-            }
-          </CardText>
-        </CardBody>
-      </Card>
-
-      <Row style={{marginTop:"20px"}}>
+    <div className="teacherside" style={{ marginRight: "70px" }}>
+      <div className="teacher-header">
+      <Alert
+        style={{
+          fontSize: "30px",
+          marginTop: "2%",
+          marginRight: "2%",
+          marginLeft: "2%",
+        }}
+      >
         {
-          state.courses.TeacherCourses ?
-            state.courses.TeacherCourses.map((course, i) => (
-              <Link to={`/course/${course['course id']}`}>
-              <Col key={i} sm="3">
-                <Card key={i} body>
-                <CardImg top width="100%" src={course['course Image']} alt="Card image cap" />
+                `Welcome : ${state.courses.TeacherName}`
+              }
+      </Alert>
+      </div>
+      <div className="teacher-cards">
 
-                  <CardTitle style={{ fontWeight: 500 }} tag="h4">
-                    {course['course Name ']}
-                  </CardTitle>
-                  <CardText>
-                    {course['course Description']}
-                  </CardText>
-                </Card>
-              </Col>
-              </Link>
-            )) : null
-        }
-      </Row>
+          {
+            state.courses.TeacherCourses ?
+              state.courses.TeacherCourses.map((course, i) => (
+                <Link key={i} to={`/course/${course['course id']}`} >
+                    <Card key={i} body className="teacherCard" style={{ width: "20rem", height: "28rem" }}>
+                      <CardImg top width="60%" style={{width: "20rem", height: "70%" }} src={course['course Image']} alt="Card image cap" />
+                      <CardTitle style={{ fontWeight: 500 }} tag="h1">
+                        {course['course Name']}
+                      </CardTitle>
+                      <CardText>
+                        {course['course Description']}
+                      </CardText>
+                    </Card>
+                </Link>
+              )) : null
+          }
+      </div>
 
     </div>
 
